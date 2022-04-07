f4f0a7ef9c2486ee5b8860c60e4e2ab2e1d0da7c
JanHass <janmika.hass@student.fhws.de>
2022-04-07 11:56:09 +0200

    TMP_Daten_nicht_funktionsfähig

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,10 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/m/MessageToast",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/Device",
+	"sap/base/Log",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/MessageBox",
+	"sap/ui/thirdparty/jquery",
+	"sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (MessageToast, Controller, Device, Log, MessageBox, Filter, FilterOperator,jQuery,JSONModel) {
         "use strict";
 
         return Controller.extend("project3bestellungfreigeben.controller.Master", {

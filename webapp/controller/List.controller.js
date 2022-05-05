f5348f32b48853dad62334f1984df39678de071f
Moritz <moritz.endres@student.fhws.de>
2022-05-05 16:01:10 +0200

    List Controller: Sort implemented

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -83,23 +83,44 @@ sap.ui.define([
          * @public
          */
         onSearch: function (oEvent) {
-            if (oEvent.getParameters().refreshButtonPressed) {
-                // Search field's 'refresh' button has been pressed.
-                // This is visible if you select any list item.
-                // In this case no new search is triggered, we only
-                // refresh the list binding.
-                this.onRefresh();
-                return;
-            }
+            // if (oEvent.getParameters().refreshButtonPressed) {
+            //     // Search field's 'refresh' button has been pressed.
+            //     // This is visible if you select any list item.
+            //     // In this case no new search is triggered, we only
+            //     // refresh the list binding.
+            //     this.onRefresh();
+            //     return;
+            // }
+            
 
-            var sQuery = oEvent.getParameter("query");
+            // var sQuery = oEvent.getParameter("query");
 
-            if (sQuery) {
-                this._oListFilterState.aSearch = [new Filter("PurchaseOrder", FilterOperator.Contains, sQuery)];
-            } else {
-                this._oListFilterState.aSearch = [];
-            }
-            this._applyFilterSearch();
+            // if (sQuery) {
+            //     var aFilters = []
+            //     this._oListFilterState.aSearch = [new Filter("Supplier", FilterOperator.Contains, sQuery)];
+            //     this._oListFilterState.aSearch = [new Filter("PurchaseOrder", FilterOperator.Contains, sQuery)];
+            // } else {
+            //     this._oListFilterState.aSearch = [];
+            // }
+            // this._applyFilterSearch();
+
+            // add filter for search
+			var aFilters = [];
+			var sQuery = oEvent.getSource().getValue();
+			if (sQuery && sQuery.length > 0) {
+                var filterSupplier = new Filter("Supplier", FilterOperator.Contains, sQuery);
+				var filterPurchaseOrder = new Filter("PurchaseOrder", FilterOperator.Contains, sQuery);
+				aFilters.push(filterPurchaseOrder);
+                aFilters.push(filterSupplier)
+			}
+            
+
+			// update list binding
+			var oList = this.byId("list");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilters, "Application");
+            
+            
 
         },
 

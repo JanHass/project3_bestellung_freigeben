4448e12c75a4a01526a315bccf209f588c7a2d12
Julia Fuchs <julia.fuchs@student.fhws.de>
2022-06-07 09:54:26 +0200

    USA + Anpassungen Grafisch

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -2,25 +2,18 @@ sap.ui.define([
     "./BaseController",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
-    "sap/m/library"
-], function (BaseController, JSONModel, formatter, mobileLibrary) {
+  
+    
+], function (BaseController, JSONModel, formatter) {
     "use strict";
 
-    // shortcut for sap.m.URLHelper
-    var URLHelper = mobileLibrary.URLHelper;
-
-    return BaseController.extend("odatatmp1.controller.Detail", {
+   return BaseController.extend("odatatmp1.controller.Detail", {
 
         formatter: formatter,
 
-        /* =========================================================== */
-        /* lifecycle methods                                           */
-        /* =========================================================== */
+       
 
         onInit: function () {
-            // Model used to manipulate control states. The chosen values make sure,
-            // detail page is busy indication immediately so there is no break in
-            // between the busy indication for loading the view's meta data
             var oViewModel = new JSONModel({
                 busy : false,
                 delay : 0,
@@ -33,58 +26,17 @@ sap.ui.define([
 
             this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
         },
+        boldText: function(){
+            var text= sap.ui.getCore().byId("txtBold");
+            var name=this.getView().getModel("data").getProperty("/name");
 
-        /* =========================================================== */
-        /* event handlers                                              */
-        /* =========================================================== */
-
-        /**
-         * Event handler when the share by E-Mail button has been clicked
-         * @public
-         */
-        onSendEmailPress: function () {
-            var oViewModel = this.getModel("detailView");
-
-            URLHelper.triggerEmail(
-                null,
-                oViewModel.getProperty("/shareSendEmailSubject"),
-                oViewModel.getProperty("/shareSendEmailMessage")
-            );
+         $("#txtBold").prepend('"'+"<b>"+name+"</b>"+'"');
         },
 
-        
-        /**
-         * Updates the item count within the line item table's header
-         * @param {object} oEvent an event containing the total number of items in the list
-         * @private
-         */
-        onListUpdateFinished: function (oEvent) {
-            var sTitle,
-                iTotalItems = oEvent.getParameter("total"),
-                oViewModel = this.getModel("detailView");
 
-            // only update the counter if the length is final
-            if (this.byId("lineItemsList").getBinding("items").isLengthFinal()) {
-                if (iTotalItems) {
-                    sTitle = this.getResourceBundle().getText("detailLineItemTableHeadingCount", [iTotalItems]);
-                } else {
-                    //Display 'Line Items' instead of 'Line items (0)'
-                    sTitle = this.getResourceBundle().getText("detailLineItemTableHeading");
-                }
-                oViewModel.setProperty("/lineItemListTitle", sTitle);
-            }
-        },
 
-        /* =========================================================== */
-        /* begin: internal methods                                     */
-        /* =========================================================== */
+    
 
-        /**
-         * Binds the view to the object path and expands the aggregated line items.
-         * @function
-         * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-         * @private
-         */
         _onObjectMatched: function (oEvent) {
             var sObjectId =  oEvent.getParameter("arguments").objectId;
             this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
@@ -96,13 +48,6 @@ sap.ui.define([
             }.bind(this));
         },
 
-        /**
-         * Binds the view to the object path. Makes sure that detail view displays
-         * a busy indicator while data for the corresponding element binding is loaded.
-         * @function
-         * @param {string} sObjectPath path to the object to be bound to the view.
-         * @private
-         */
         _bindView: function (sObjectPath) {
             // Set busy indicator during view binding
             var oViewModel = this.getModel("detailView");
@@ -136,20 +81,7 @@ sap.ui.define([
                 this.getOwnerComponent().oListSelector.clearListListSelection();
                 return;
             }
-
-            var sPath = oElementBinding.getPath(),
-                oResourceBundle = this.getResourceBundle(),
-                oObject = oView.getModel().getObject(sPath),
-                sObjectId = oObject.PurchaseOrder,
-                sObjectName = oObject.PurchaseOrder,
-                oViewModel = this.getModel("detailView");
-
-            this.getOwnerComponent().oListSelector.selectAListItem(sPath);
-
-            oViewModel.setProperty("/shareSendEmailSubject",
-                oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-            oViewModel.setProperty("/shareSendEmailMessage",
-                oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+               
         },
 
         _onMetadataLoaded: function () {
@@ -173,32 +105,6 @@ sap.ui.define([
             oViewModel.setProperty("/busy", true);
             // Restore original busy indicator delay for the detail view
             oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
-        },
-
-        /**
-         * Set the full screen mode to false and navigate to list page
-         */
-        onCloseDetailPress: function () {
-            this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", false);
-            // No item should be selected on list after detail page is closed
-            this.getOwnerComponent().oListSelector.clearListListSelection();
-            this.getRouter().navTo("list");
-        },
-
-        /**
-         * Toggle between full and non full screen mode.
-         */
-        toggleFullScreen: function () {
-            var bFullScreen = this.getModel("appView").getProperty("/actionButtonsInfo/midColumn/fullScreen");
-            this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", !bFullScreen);
-            if (!bFullScreen) {
-                // store current layout and go full screen
-                this.getModel("appView").setProperty("/previousLayout", this.getModel("appView").getProperty("/layout"));
-                this.getModel("appView").setProperty("/layout", "MidColumnFullScreen");
-            } else {
-                // reset to previous layout
-                this.getModel("appView").setProperty("/layout",  this.getModel("appView").getProperty("/previousLayout"));
-            }
         }
     });
 
